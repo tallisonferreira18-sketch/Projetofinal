bada9a6ada6b380483662ea839d6138bca504992
tallisonferreira18-sketch <tallisonferreira18@gmail.com>
2025-11-18 13:55:25 -0300

    Add additional attributes to personagem cadastro

--- Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/personagem-cadastro.php
+++ Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/personagem-cadastro.php
@@ -8,9 +8,15 @@
         $especie = $_POST["especie"];
         $classe = $_POST["classe"];
         $subclasse = $_POST["subclasse"];
+        $forca = $_POST["forca"];
+        $destreza = $_POST["destreza"];
+        $constituicao = $_POST["constituicao"];
+        $inteligencia = $_POST["inteligencia"];
+        $sabedoria = $_POST["sabedoria"];
+        $carisma = $_POST["carisma"];
 
-        $sql = "INSERT INTO personagens (personagem, jogador, especie, classe, subclasse)
-            VALUES ('$personagem', '$jogador', '$especie', '$classe', '$subclasse')";
+       $sql = "INSERT INTO personagens (personagem, jogador, especie, classe, subclasse, forca, destreza, constituicao, inteligencia, sabedoria, carisma)
+            VALUES ('$personagem', '$jogador', '$especie', '$classe', '$subclasse', '$forca', '$destreza', '$constituicao','$inteligencia', '$sabedoria', '$carisma')";
         $executa = mysqli_query($conexao, $sql);
         if($executa) {
             echo "<h2>Cadastro realizado com sucesso.</h2>";
@@ -22,4 +28,5 @@
     }else{
         echo "<h2>Acesso negado.</h2>";
         echo "<a href='?pg=personagem-admin'>Voltar</a>";
+
     }
