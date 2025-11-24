6cdfcb1aaa59cc9023a2ad100b60779239448da1
tallisonferreira18-sketch <tallisonferreira18@gmail.com>
2025-11-24 16:34:56 -0300

    Delete Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin directory

--- Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/admin/principal.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    echo "<h2>Bem-vindo ao painel principal!</h2>";
-
-?>
--- Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //1ª etapa - conectando com servidor
-    $conexao = mysqli_connect("localhost","root","");
-    //2ª etapa - selecionando banco de dados
-    $db = mysqli_select_db($conexao,"projeto_1_tallison_f_miranda_");
-
-    if(!$conexao){
-        echo "<h2>Erro ao conectar o banco de dados</h2>";
-    }
