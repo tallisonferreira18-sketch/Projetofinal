acacc8f03662a534a617771642a33a45be08a8eb
tallisonferreira18-sketch <tallisonferreira18@gmail.com>
2025-12-02 17:39:35 -0300

    Update database connection details in config

--- Projeto1 - Tallison F. Miranda/admin/config.inc.php
+++ Projeto1 - Tallison F. Miranda/admin/config.inc.php
@@ -1,6 +1,7 @@
 <?php
-    $conexao = mysqli_connect("127.0.0.1","root","","projeto_1_tallison_f_miranda_",3306);
+    $conexao = mysqli_connect("127.0.0.1","root","","projetofinal_ded",3306);
 
     if(!$conexao){
         echo "<h2>Erro ao conectar o banco de dados: " . mysqli_connect_error() . "</h2>";
+
     }
