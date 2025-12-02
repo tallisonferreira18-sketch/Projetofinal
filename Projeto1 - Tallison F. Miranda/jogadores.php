b265ee0bcd5ace4c1e7ada1b388157e2810f606a
Arthur Nunes <arthur.nunes.rodrigues@gmail.com>
2025-12-02 19:05:23 -0300

    Delete jogadores.php

--- Projeto1 - Tallison F. Miranda/jogadores.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Esta página foi unificada com 'personagens.php'.
- * O código abaixo redireciona o usuário para a página correta,
- */
-header("Location: ?pg=personagens");
-exit; // Garante que nenhum outro código seja executado após o redirecionamento.
-?>
